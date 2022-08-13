d43b60686027ac20c295028884bdbf7c0a6d1f2f
Serg89spb <serg89spb@gmail.com>
2022-08-13 22:13:27 +0300

    15.1 - FX Emissive. End Building

--- Source/BuildingCubes/Private/Blocks/BC_C_BaseBlock.cpp
+++ Source/BuildingCubes/Private/Blocks/BC_C_BaseBlock.cpp
@@ -11,13 +11,49 @@ ABC_C_BaseBlock::ABC_C_BaseBlock()
 	SetRootComponent(BC_MeshComponent);
 }
 
+void ABC_C_BaseBlock::OnEndBuilding()
+{
+	M_StartFadeEmit = true;
+	if(IsValid(BC_MeshComponent) && IsValid(BC_MeshComponent->GetMaterial(0)))
+	{
+		M_BlockDynMat = Cast<UMaterialInstanceDynamic>(BC_MeshComponent->GetMaterial(0));
+	}
+}
+
 void ABC_C_BaseBlock::BeginPlay()
 {
 	Super::BeginPlay();
+	M_FXEmitPower = FadeTime;
 }
 void ABC_C_BaseBlock::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if(M_StartFadeEmit)
+	{
+		if(M_FXEmitPower > 0)
+		{
+			M_FXEmitPower -= DeltaTime;
+			ChangeEmitPower(M_FXEmitPower * FadePowerMult);
+		}
+		else
+		{
+			ResetEmitToZero();
+		}
+	}
+
+}
+
+void ABC_C_BaseBlock::ChangeEmitPower(float Power)
+{
+	if(!IsValid(M_BlockDynMat)) return;
+	M_BlockDynMat->SetScalarParameterValue(FName("Emit Power"),Power * Power);
+}
+
+void ABC_C_BaseBlock::ResetEmitToZero()
+{
+	ChangeEmitPower(0.0f);
+	M_FXEmitPower = 0.0f;
+	M_StartFadeEmit = false;
 }
 
--- Source/BuildingCubes/Public/Blocks/BC_C_BaseBlock.h
+++ Source/BuildingCubes/Public/Blocks/BC_C_BaseBlock.h
@@ -10,17 +10,41 @@ UCLASS()
 class BUILDINGCUBES_API ABC_C_BaseBlock : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
 	ABC_C_BaseBlock();
 
-	UPROPERTY(EditDefaultsOnly,BlueprintReadWrite,Category="Components")
+	void OnEndBuilding();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Components")
 	UStaticMeshComponent* BC_MeshComponent;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Emit")
+	float FadeTime = 1.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Emit")
+	float FadePowerMult = 10.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Emit")
+	float RiseTime = 1.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Emit")
+	float RisePowerMult = 10.0f;
 
 protected:
 	virtual void BeginPlay() override;
 
-public:	
+public:
 	virtual void Tick(float DeltaTime) override;
 
+private:
+	bool M_StartFadeEmit;
+
+	float M_FXEmitPower;
+
+	UPROPERTY()
+	UMaterialInstanceDynamic* M_BlockDynMat;
+
+	void ChangeEmitPower(float Power);
+	void ResetEmitToZero();
 };
